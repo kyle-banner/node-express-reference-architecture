2187af67235f1923a851a5845620b7c532ecc2b6
Kyle Banner <kyle.banner@slalom.com>
2020-03-27 14:24:01 -0600

    Use dotenv for environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 const app = express();
-const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
 // start the Express server
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   // tslint:disable-next-line:no-console
-  console.log(`server started at http://localhost:${port}`);
+  console.log(`server started at http://localhost:${process.env.PORT}`);
 });
