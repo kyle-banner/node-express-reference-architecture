4a96165c7c9f2ddf40456e628ef4d79b2990ddb1
Kyle Banner <kyle.d.banner@gmail.com>
2020-07-23 23:46:45 -0600

    Add initial sql client

---
+++ src/util/sqlDatabaseClient.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from 'sequelize-typescript';
+import { Container } from 'typedi';
+import * as path from 'path';
+
+export default class Database {
+  private configureSequelize: Sequelize;
+  constructor() {
+    this.configureSequelize = new Sequelize({
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+      modelPaths: [path.resolve(__dirname, '../repository/model/')]
+    });
+  }
+  getSequelize() {
+    return this.configureSequelize;
+  }
+}
