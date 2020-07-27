af72ea9cd35c51e20cad2ff938b179a92cd10d9b
Kyle Banner <kyle.d.banner@gmail.com>
2020-07-27 10:05:59 -0600

    Testing not using package.json to start node

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,4 +1,4 @@
 # If the directory, `dist`, doesn't exist, create `dist`
 stat dist || mkdir dist
 # Archive artifacts
-zip dist/$npm_package_name.zip -r build package.json package-lock.json
+zip dist/$npm_package_name.zip -r build package.json package-lock.json Procfile
